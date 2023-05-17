8b3b8ac274e73b6730d5598c2fa36992f27efe17
ksama22 <ksama22@jaumebalmes.net>
2023-05-17 20:51:11 +0200

    ultim

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 let API_BASE = 'https://api.nasa.gov';
-let API_KEY = 'yns2Tz49qdSIKPohDnbeNoeRfCIrqRtt5smy1cnM'
+let API_KEY = 'yns2Tz49qdSIKPohDnbeNoeRfCIrqRtt5smy1cnM';
+
+
 //Controla el zoom
 let zoomPressed = false;
 let zoomxyz = { x: 0, y: 0, z: 0 }
 let speedCamera = 1;
 let llistaMeteorids = null;
-let uidLastMetorid = null;
+let uidEarh = null;
 let actualSelect = 0;
+
+let listAsteroids = []
+
+
+window.onload = function () {
+    //   findPhoto();
+    console.log("Carregat");
+}
 
 //del treejs 
 
@@ -18,7 +28,7 @@ let cvSize = { "height": 400, "width": 800 }
 
 //Vigila la camara
 camera = new THREE.PerspectiveCamera(50, 1, 1, 1000);
-camera.position.z = 300;
+camera.position.z = 1000;
 camera.position.x = 0;
 camera.position.y = 0;
 
@@ -34,47 +44,20 @@ renderer.setSize(cvSize.width, cvSize.height,);
 document.getElementById("escenario").appendChild(renderer.domElement);
 //window.addEventListener('resize', onWindowResize(renderer,camera));
 
-let mesh = createMeshPlanet(20, 30, 30, 'textures/4k.jpg');
-//F7A49AAF-FF41-4A69-BF89-62FFE62C7DB7
+//Creacio de la terra
+let mesh = createMeshPlanet(400, 30, 30, 'textures/4k.jpg');
 mesh.position.y = 0
 mesh.position.x = 0
-console.log(mesh.uuid);
-/*
-for (let i = 0; i < 5; i++) {
-    let meshn = createMeshPlanet(20 + i, 30, 30, 'textures/mars.png');
-    meshn.position.x = (i + 1) * 50;
-    scene.add(meshn);
-    animate(meshn);
-
-}*/
-
-//Afegeix el planeta
-scene.add(mesh);
-
-
-//Para per parametre el mesh
+//Identificador global de la terra
+uidEarh = mesh.uuid;
 animate(mesh);
-
-
-function getData() {
-    fetch(API_URL, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-        },
-    })
-        .then(response => response.json())
-        .then(response => console.log(JSON.stringify(response)))
-    console.log("hi");
-}
-
 
 
 function findDate() {
     document.getElementById("loading").style.display = "block";
     let inici = document.getElementById("start-date").value;
-    //let query = `${API_BASE}/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=yns2Tz49qdSIKPohDnbeNoeRfCIrqRtt5smy1cnM`;
-    let query = `${API_BASE}/neo/rest/v1/feed?start_date=${inici}&end_date=${inici}&api_key=${API_KEY}`;
+    //let query = `${API_BASE}/neo/rest/v1/feed?start_date=${inici}&end_date=${inici}&api_key=${API_KEY}`;
+    let query = "hola.json";
     console.log(query);
     fetch(query, {
         method: 'GET',
@@ -83,7 +66,7 @@ function findDate() {
         },
     })
         .then(response => response.json())
-        .then(response => listAsteroids(response, inici))
+        .then(response => getAsteroids(response, inici))
 
 }
 
@@ -103,43 +86,137 @@ function findPhoto() {
 }
 
 //Coding a 3D Solar System with JavaScript + Three.js  https://www.youtube.com/watch?v=KOSMzSyiEiA
-function listAsteroids(asteroid, date) {
-    //Esborra l'anterior
-
-    llistaMeteorids = asteroid.near_earth_objects["1996-01-01"][actualSelect];
+function getAsteroids(asteroid, date) {
     document.getElementById("loading").style.display = "none";
-    let pare = document.getElementById("meteoritos");
-    pare.innerHTML = "";
-    drawMeteroid(0)
+    // L'objecte com a tal te el [1998-12-31], exemple  com obj.
+    llistaMeteorids = asteroid.near_earth_objects[date];
+    //Llista neta de tots els meteorits
+    llistaMeteorids.forEach(data => {
+        //Guardo les dades en un objecte Meteorid
+        let meteoridOBJ = new Meteoroid(
+            data.neo_reference_id,
+            data.name,
+            data.absolute_magnitude_h,
+            data.estimated_diameter.meters.estimated_diameter_min,
+            data.estimated_diameter.meters.estimated_diameter_max,
+            data.is_potentially_hazardous_asteroid,
+            data.close_approach_data[0].relative_velocity.kilometers_per_hour,
+            data.close_approach_data[0].miss_distance.kilometers,
+            data.close_approach_data[0].orbiting_body,
+            //Creo per defecte DOS mesh unic per cada meteorit
+            [createMeshForMeteorid(data.estimated_diameter.meters.estimated_diameter_min),
+            createMeshForMeteorid(data.estimated_diameter.meters.estimated_diameter_min)
+            ]
+        )
+        //Guarda el llista global
+        listAsteroids.push(meteoridOBJ);
+    });
+    //Omple els selectors amb la llista
+    fillSelector();
+    fillTaula();
 
+    drawMeteroid();
+    console.log("mi lista", listAsteroids);
+
+}
+
+function fillSelector() {
+    let pare1 = document.getElementById("selector1");
+    let pare2 = document.getElementById("selector2");
+
+    deleteChild(pare1);
+    deleteChild(pare2);
+
+    for (let i = 0; i < listAsteroids.length; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.innerText = listAsteroids[i].name;
+        pare1.appendChild(option);
+        //Omple el primer i segon select a l'hora amb els mateixos elements
+        let option2 = document.createElement("option");
+        option2.value = i;
+        option2.innerText = listAsteroids[i].name;
+        pare2.appendChild(option2);
+    }
+
+
+}
+
+function fillTaula() {
+    //Agafa el valor del selector
+    let nSelect = parseInt(document.getElementById("selector1").value);
+    let th = document.getElementById("thGenerated");
+    let td = document.getElementById("tdGenerated");
+
+    //Agafa el valor del selector
+    let nSelect2 = parseInt(document.getElementById("selector2").value);
+    let th2 = document.getElementById("thGenerated2");
+    let td2 = document.getElementById("tdGenerated2");
+
+    //Codi repetit es posa en funcio per omplir taula
+    fillChildTaula(th, td, nSelect, 0)
+    fillChildTaula(th2, td2, nSelect2, 1)
+}
+
+function fillChildTaula(th, td, nSelect, numMesh) {
+    //Como no renderitza el carrego els fills per tornar a generar
+    deleteChild(th);
+    deleteChild(td);
+    //La tindra l'objecte {"algo":"valor"} amb Object.keys agafa el "algo"
+    Object.keys(listAsteroids[nSelect]).forEach(element => {
+        let child = document.createElement("th");
+        child.innerText = element;
+        th.appendChild(child);
+    });
+
+    //La tindra l'objecte {"algo":"valor"} amb Object.values agafa el "valor"
+    Object.values(listAsteroids[nSelect]).forEach(element => {
+        let child = document.createElement("td");
+        child.innerText = element;
+        td.appendChild(child);
+    });
+
+    console.log("cambiame");
+}
+
+
+function deleteChild(parentElement) {
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
 }
 
 //Pinta un asteorid
-function drawMeteroid(num) {
-    actualSelect = actualSelect + num;
-    let obj = llistaMeteorids;
-    //Esborra l'anterior existent per 'uid'
-    if (uidLastMetorid !== null) {
-        scene.remove(scene.getObjectByProperty('uuid', uidLastMetorid));
-    }
-    let size = llistaMeteorids.estimated_diameter.kilometers.estimated_diameter_min;
-    console.log("size", size);
-    console.log(obj.name);
-    console.log(obj.estimated_diameter.meters.estimated_diameter_min);
-    console.log(obj.close_approach_data[0].epoch_date_close_approach); //close_approach_date_full
-    //{obj.estimated_diameter.meters.estimated_diameter_min}
-    //${obj.name}
-    let mesh = createMeshPlanet(size * 10, 30, 30, 'textures/stone.png');
-    mesh.position.y = 0;
-    mesh.position.x = 50;
-    uidLastMetorid = mesh.uuid;
-    animate(mesh);
-    console.log(mesh.uuid);
-    //Afegeix el planeta
-    scene.add(mesh);
-    console.log(actualSelect, "defv");
+function drawMeteroid() {
+
+    //Cada mesh te un identificador, i es por esborrar de la escena aixi
+    /* listAsteroids.forEach(element => {
+         scene.remove(scene.getObjectByProperty('uuid', element.mesh.uuid));
+     });
+ */
+
+    let slct1 = parseInt(document.getElementById("selector1").value);
+    let slct2 = parseInt(document.getElementById("selector2").value);
+    scene.remove(scene.getObjectByProperty('uuid', listAsteroids[slct1].meshOBJ[0].uuid));
+    scene.remove(scene.getObjectByProperty('uuid', listAsteroids[slct2].meshOBJ[1].uuid));
+
+    let meshAsteroid1 = listAsteroids[slct1].meshOBJ;
+    let meshAsteroid2 = listAsteroids[slct2].meshOBJ;
+
+    meshAsteroid1[0].position.y = 0;
+    meshAsteroid2[1].position.y = 0;
+
+    meshAsteroid1[0].position.x = 0;
+    meshAsteroid2[1].position.x = 500;
+
+    scene.add(meshAsteroid1[0]);
+    scene.add(meshAsteroid2[1]);
+
+
+
 
 }
+
 
 
 
@@ -229,6 +306,52 @@ function getDayPhoto(props) {
     document.getElementById("description").innerText = props.explanation;
 }
 
-window.onload = function () {
-    findPhoto();
+
+class Meteoroid {
+    constructor(neoReference, name, magnitude, minDiameter, maxDiameter, isDangerous, velocity, distance, orbiting, meshOBJ) {
+        this.neoReference = neoReference;
+        this.name = name;
+        this.magnitude = magnitude;
+        this.minDiameter = minDiameter;
+        this.maxDiameter = maxDiameter;
+        this.isDangerous = isDangerous;
+        this.velocity = velocity;
+        this.distance = distance;
+        this.orbiting = orbiting;
+        //El mesh es el sprite que despres es dibuixa sobre l'escena
+        this.meshOBJ = meshOBJ;
+    }
+}
+
+//Preceo un mesh
+function createMeshForMeteorid(size) {
+    let mesh = createMeshPlanet(size, 30, 30, 'textures/stone.png');
+    mesh.position.y = 0;
+    mesh.position.x = 50;
+    animate(mesh);
+    return mesh;
+}
+
+function generaNovesDades() {
+    fillTaula()
+    drawMeteroid();
+}
+
+function generaEarth(event) {
+    // se que puedo por id 'tierra' pero con el event tambien sirve
+    if (event.target.checked) {
+        scene.add(mesh);
+        return;
+    }
+    scene.remove(scene.getObjectByProperty('uuid', uidEarh));
+}
+
+//Canvia el materials dels asteroides, agafa el value del select que es la ruta de les textures
+function changeMaterial(num, event) {
+    let opcio = event.target.value;
+    listAsteroids.forEach(element => {
+        //Mesh 0 meteorit esquerra, mesh 1 meteorit dreta
+        element.meshOBJ[num].material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(opcio) });
+    });
+
 }
